90bd8e1da2d899627f0e4bf5e73267667bdd0bbf
nithya279 <nithyanandreddy7@gmail.com>
2021-05-02 22:35:11 +0530

    CourseService

--- LearnTodayWebApi1/LearnTodayWebApi1/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace LearnTodayWebApi1.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- StudentService/StudentService/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace StudentService.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
