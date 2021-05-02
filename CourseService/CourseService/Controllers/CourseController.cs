3f8f02a5bb8e1aea5d014d55c690ef68039ddf0e
nithya279 <nithyanandreddy7@gmail.com>
2021-05-02 22:51:18 +0530

    CourseService

---
+++ CourseService/CourseService/Controllers/CourseController.cs
@@ -0,0 +1,37 @@
+﻿using CourseService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CourseService.Controllers
+{
+    public class CourseController : ApiController
+    {
+        static List<Course> courses = new List<Course>()
+        {
+            new Course(){CourseId=1,CourseName="Android",Trainer="Shawn",Fees=12000,Description="Android is a mobile operating system dev"},
+             new Course(){CourseId=2,CourseName="Asp.net",Trainer="rio",Fees=20000,Description="Good performance on riding"}
+        };
+        public HttpResponseMessage Get([FromUri]string coursename)
+        {
+            var findedCourse = courses.Where(c => c.CourseName == coursename).SingleOrDefault();
+            if(findedCourse==null)
+            {
+               return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Course Name " +coursename+" not found");
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, findedCourse,"application/json");
+            }
+        }
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] Course course)
+        {
+            courses.Add(course);
+            return Request.CreateResponse(HttpStatusCode.OK, course);
+        }
+    }
+}
---
+++ CourseService/CourseService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CourseService.WebApiApplication" Language="C#" %>
