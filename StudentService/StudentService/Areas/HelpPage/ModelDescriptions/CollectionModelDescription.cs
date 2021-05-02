88c6307ff384040fa928eb0886fa74d2611e76c7
nithya279 <83126758+nithya279@users.noreply.github.com>
2021-05-02 22:19:12 +0530

    Delete StudentService directory

--- StudentService/StudentService/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace StudentService.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
