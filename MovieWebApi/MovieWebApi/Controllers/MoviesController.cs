6651f395e3ecfa19571d9eb84eee9a487691cc7e
nithya279 <83126758+nithya279@users.noreply.github.com>
2021-05-02 18:16:41 +0530

    Add files via upload

---
+++ MovieWebApi/MovieWebApi/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieWebApi.Controllers
+{
+    public class MoviesController : ApiController
+    {
+      /*  public string Get()
+        {
+            return "Hello from WebApi";
+        }*/
+        [HttpGet]
+        public List<string> GetMovies()
+        {
+            return new List<string>{"Titanic","MI","Matrix"};
+        }
+    }
+}
