eef226e52b23de6acfc8e6ff55023d0e07481580
nithya279 <83126758+nithya279@users.noreply.github.com>
2021-05-02 22:18:51 +0530

    Delete MovieWebApi directory

--- MovieWebApi/MovieWebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MovieWebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- MovieWebApi/MovieWebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MovieWebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
